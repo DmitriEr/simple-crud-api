ddc588f1ca8f73c105696fa0c29c949c2cb0596b
Dmitrii Erkin <dmitrii.erkin@simbirsoft.com>
2021-11-26 20:33:10 +0400

    feat: add delete response

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
 const http = require('http');
+const path = require('path');
+const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
-const persons = [];
+const dataPath = path.join(__dirname, 'db', 'database.txt');
 
-const checkData = (data, url, method) => {
-    const { name, age, hobbies } = data;
-    if (!name) {
-        throw new Error('name is required');
-    }
-    if (!age) {
-        throw new Error('age is required');
-    }
-    if (!hobbies) {
-        throw new Error('hobbies is required');
-    }
-    if (!/\/person\//.test(url) && (method === 'POST')) {
-        throw new Error('invalid path');
-    }
-}
+let persons = [];
+
+fs.readFile(dataPath, 'utf-8', (err, data) => {
+    persons = JSON.parse(data.toString());
+});
 
 const server = http.createServer((req, res) => {
     switch(req.method) {
@@ -51,8 +43,24 @@ const server = http.createServer((req, res) => {
                 res.end(JSON.stringify(requestData));
             })
             break;
-        case 'DELETE':
-            res.statusCode = 200;
+        case 'DELETE': {
+            let statusCode = 204;
+            req.on('data', () => {
+                    const [id] = req.url.match(/[\d\w]{8}-([\d\w]{4}-){3}[\d\w]{12}/);
+                    if (id) {
+                        const [user] = persons.filter((item) => item.id === id); 
+                        if (!user) {
+                            statusCode = 404;
+                        } else {
+                            persons = persons.filter((item) => item.id !== id);
+                        }
+                    }
+            })
+            req.on('end', () => {
+                res.statusCode = statusCode;
+                res.end();
+            });
+        }
             break;
         case 'PUT':
             res.statusCode = 200;
@@ -68,14 +76,15 @@ const server = http.createServer((req, res) => {
                 requestData = { detail: 'invalid path'}
             } else {
                 if (/[\d\w]{8}-([\d\w]{4}-){3}[\d\w]{12}(\/)?/.test(req.url)) {
-                    // const isUserExist = persons.filter((data) => data.id === id);
-                    // if (!isUserExist) {
-                    //     statusCode = 404;
-                    //     requestData = { detail: 'user not exist'}
-                    // } else {
-                    //     statusCode = 200;
-                    //     requestData = { isUserExist };
-                    // }
+                    const [id] = req.url.match(/[\d\w]{8}-([\d\w]{4}-){3}[\d\w]{12}/);
+                    const [user] = persons.filter((data) => data.id === id);
+                    if (!user) {
+                        statusCode = 404;
+                        requestData = { detail: 'user not exist'}
+                    } else {
+                        statusCode = 200;
+                        requestData = user;
+                    }
                 } else {
                     statusCode = 201;
                     requestData = persons;
